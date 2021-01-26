417171c20a39d5495827332d17be6e1d6c7592fc
chaejonghun <ace1373@naver.com>
2021-01-26 19:14:21 +0900

    217페이지까지 끝남

--- src/Info.js
+++ src/Info.js
@@ -1,29 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
+import useInputs from "/.useInputs";
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
           <b>이름:</b> {name}
         </div>
         <div>
-          <b>닉네임:</b> {nickname}
+          <b>닉네임: </b>
+          {nickname}
         </div>
       </div>
     </div>
